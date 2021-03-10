b43bef57e4d40ade779f72c41647272ccc9b07f6
MDLutoronto <gis.maps@utoronto.ca>
2021-03-10 11:27:32 -0500

    uploading copyfiles

--- copyfiles.sh
+++ copyfiles.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-for file in `cat filename.txt` #where filename is a text file containing filenames and their locations. one filename per line
+for file in `cat filename.txt` 
+#where filename is a text file containing filenames and their locations. one filename per line
   do cp $file folderlocation/
 done
+
